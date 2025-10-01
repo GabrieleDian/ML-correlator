1c12d48c29b61f3c61f981fa0bbf84bd96e104ce
Reza Doobary <rezadoobary@Rezas-MacBook-Pro.local>
2025-10-01 09:49:23 +0100

    adding some fgraph uitlity

---
+++ Tree_classifier_for_graphs/new_stuff/runner2.sh
@@ -0,0 +1,7 @@
+python "/Users/rezadoobary/Documents/MLCorrelator2/ML-correlator/Tree_classifier_for_graphs/new_stuff/fgraph_features_cli.py" \
+  --input "/Users/rezadoobary/Documents/MLCorrelator2/ML-correlator/Graph_Edge_Data/graph_data_7.csv" \
+  --output "/Users/rezadoobary/Documents/MLCorrelator2/ML-correlator/Tree_classifier_for_graphs/new_stuff/features/fgraphs/7loops.csv" \
+  --groups basic,connectivity,centrality,core,robustness,cycles,spectral_laplacian,spectral_adjacency,netlsd,planarity,symmetry,community,motifs34,motifs5,induced4,induced5,tda \
+  --batch-size 10 \
+  --batch-dir "/Users/rezadoobary/Documents/MLCorrelator2/ML-correlator/Tree_classifier_for_graphs/new_stuff/features/fgraphs/7loops" \
+  --keep-coeff-first
