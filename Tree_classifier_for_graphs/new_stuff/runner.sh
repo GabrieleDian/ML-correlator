1fe523ef8cb279d61d12407f889f4a95b0fb96f0
Reza Doobary <rezadoobary@Rezas-MacBook-Pro.local>
2025-10-19 15:48:32 +0100

    creating new motifs function only with much better commenting and docs

--- Tree_classifier_for_graphs/new_stuff/runner.sh
+++ Tree_classifier_for_graphs/new_stuff/runner.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-for i in 8 9; do
+for i in 6; do
   echo ">> Processing i=$i"
   python "/Users/rezadoobary/Documents/MLCorrelator2/ML-correlator/Tree_classifier_for_graphs/new_stuff/fgraph_features_cli2.py" \
     --input "/Users/rezadoobary/Documents/MLCorrelator2/ML-correlator/Graph_Edge_Data/graph_data_${i}.csv" \
