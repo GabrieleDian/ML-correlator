e2672f0fc472534cba2e17ab3bcba2909a04c7a8
Reza Doobary <rezadoobary@Rezas-MacBook-Pro.local>
2025-10-23 08:00:40 +0100

    update

--- Tree_classifier_for_graphs/new_stuff/runner_motifs.sh
+++ Tree_classifier_for_graphs/new_stuff/runner_motifs.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-for i in 10; do
+for i in 5 6 7 8 9 10; do
   echo ">> Processing i=$i"
   python "/Users/rezadoobary/Documents/MLCorrelator2/ML-correlator/Tree_classifier_for_graphs/new_stuff/motif_features_cli.py" \
-    --input "/Users/rezadoobary/Documents/MLCorrelator2/ML-correlator/Graph_Edge_Data/graph_data_${i}.csv" \
-    --output "/Users/rezadoobary/Documents/MLCorrelator2/ML-correlator/Tree_classifier_for_graphs/new_stuff/features/fgraphs/${i}loops.csv" \
-    --groups spectral,motifs34,motifs5,induced4,induced5,cross_layer \
+    --input "/Users/rezadoobary/Documents/MLCorrelator2/ML-correlator/Graph_Edge_Data/den_graph_data_${i}.csv" \
+    --output "/Users/rezadoobary/Documents/MLCorrelator2/ML-correlator/Tree_classifier_for_graphs/new_stuff/features/motif_features_den/${i}loops.csv" \
+    --groups spectral,motifs34,motifs5,induced4,induced5 \
     --workers 8 
 done
 
