d4b4dfe5f5ba40c773ab4f277c76d5111150ed48
RCDoobary <doobaryreza@gmail.com>
2025-12-01 18:52:28 +0000

    update

--- Tree_classifier_for_graphs/new_stuff/run_script.py
+++ Tree_classifier_for_graphs/new_stuff/run_script.py
@@ -1,12 +1,12 @@
-from modelling_function_instance_faster import run_bayes_shap_for_loop
+from modelling_function_instance_faster_correct import run_bayes_shap_for_loop
 
 
 #for i in [10, 11]:   
-for i in [11]:
+for i in [6,7]:
     run_bayes_shap_for_loop(
         i,
-        input_csv_template="/home/ec2-user/ML-correlator/Tree_classifier_for_graphs/new_stuff/features/new_features/new2_merged/{i}loops_merged.csv",
-        output_root="/home/ec2-user/ML-correlator/Tree_classifier_for_graphs/new_stuff/results/bayes_shap_result",
+        input_csv_template=f"/Users/rezadoobary/Documents/MLCORRELATORS/ML-correlator/Tree_classifier_for_graphs/new_stuff/features/merged/new2_merged/{i}loops_merged.csv",
+        output_root="/Users/rezadoobary/Documents/MLCORRELATORS/ML-correlator/Tree_classifier_for_graphs/new_stuff/bayes_results",
         n_calls=20,
         n_splits=5,
         n_workers=4,
